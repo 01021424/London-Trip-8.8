a62c1ef9bc05ae668f7d9d7787dc82a853cd8854
01021424 <86634164@qq.com>
2026-07-18 17:28:29 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,32 @@
-const CACHE_NAME='london-map-v1';
-const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));});
+const CACHE_NAME='london-map-v2';
+const ASSETS=[
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './assets/london_map.png',
+  './assets/london_map_dark.png',
+  './assets/fontawesome.min.css',
+  './assets/fa-regular-400.woff2',
+  './assets/fa-solid-900.woff2',
+  './assets/marker-icon.png',
+  './assets/marker-icon-2x.png',
+  './assets/marker-shadow.png'
+];
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))
+  );
+});
